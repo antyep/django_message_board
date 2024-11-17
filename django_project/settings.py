f8be4578987fd84191ad2f3740ca1e0172c75006
antyep <antyep6@gmail.com>
2024-11-17 14:43:40 +0100

    update: allowed_hosts on settings to accept heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ugry+@nb0hul3#311i2lz08yvrbm0^#nlsh=&lhnpu#mpu(#!f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
